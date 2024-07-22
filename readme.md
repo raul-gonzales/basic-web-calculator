85405d1f29fa1697d2d6a8c02d214a5f410b5e82
Raul Gonzales <ragonz1108@gmail.com>
2024-07-22 15:13:00 -0500

    Completed base calculator and functionality

--- readme.md
+++ readme.md
@@ -9,15 +9,34 @@ This project is a simple web calculator that uses all of the skills and techniqu
   - [x] Multiply
   - [x] Divide
 - [x] Perform Unit Test as functions are created
-- [ ] Track & Store operands for updating display
+- [x] Track & Store operands for updating display
   
 ##### GUI
 - [x] Buttons for digits and operators (0-9,+,-,*,/,=)
 - [x] Button to clear numbers/operator (c)
-- [ ] Display for operands
+- [x] Display for operands
 
-The *node_modules* folder was not included to avoid slow commits. To run the operators unit test files, you must have the *node_modules* file which can be obtained by going to terminal of root directory and entering the command
-```mpm i --save-dev jest```
+The *node_modules* folder was not included to avoid slow commits. To run the operators unit test files, you must have the *node_modules* file which can be obtained by navigating to the root directory and entering the command ```mpm i --save-dev jest```
 
 Once the *node_modules* folder is installed locally, enter the command ```npm test base_functions.test.js``` to run the unit test.
 
+#### *Final notes*
+The base functionalities for the calculator are implemented. For what it is meant to do (as a basic calculator), it works as intended. However, overall there are many improvements and bugs present when going beyond the scope of what the project asks for.
+
+---
+###### *Improvements and potential solutions*
+**Improvement 1** - Digits overflowing the calculator display screen when there are too many to display.
+**Possible solutions:**
+- [ ] Add a limit to the operands buffer
+- [ ] Shrink display text size as operand string count increases (*much more complex than above*)
+
+**Improvement 2** - There is a limit of two operands and users cannot keep applying the same operation to the result once the equate button is clicked.
+**Possible solutions:**
+- [ ] Generate new operand variables to hold new values as the operator buttons are clicked.
+  - [ ] This would mean a certain order must be applied to the equation (BEDMAS/PEMDAS)
+  - [ ] Complex and will need refactoring
+- [ ] Keep track of history as long as clr button is not clicked.
+  - [ ] Maintain the same operator as the operation if multiple operands will be implemented
+
+#### Appendix
+![Screenshot of the initial calculator GUI along with bullet points of my early pseudocode and thought process. This was made using app.diagrams.net](https://github.com/raul-gonzales/basic-web-calculator/images/project_online_basic_calculator.png?raw=true)
--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 // Variables to operands and operator
-let operandOne = 0;
-let operandTwo = 0;
+let operandOne = "";
+let operandTwo = "";
 let operator = "";
+let operatorClicked = false;
+
+// DOM selection
+let display = document.querySelector(".result-display");
+let digitButtons = document.querySelectorAll(".button-digit");
+let operatorButtons = document.querySelectorAll(".button-operator");
+let clearButton = document.querySelector(".button-clear");
+let equateButton = document.querySelector(".button-equate");
+
+// Add proper events to digit buttons click
+Array.from(digitButtons).forEach((digitButton) => {
+  digitButton.addEventListener("click", handleDigitClick);
+  digitButton.addEventListener("click", handleDisplayUpdate);
+});
+
+// Add proper events to operator buttons click
+Array.from(operatorButtons).forEach((operatorButton) => {
+  operatorButton.addEventListener("click", handleOperatorClick);
+  operatorButton.addEventListener("click", handleDisplayUpdate);
+});
+
+// Add proper events to the clear button
+clearButton.addEventListener("click", handleClearClick);
+clearButton.addEventListener("click", handleDisplayUpdate);
+
+// Add proper events to the equate button
+equateButton.addEventListener("click", handleEquateClick);
 
 //-----------------------------FUNCTIONS----------------------//
 
 // Operator functions
 const add = function (a, b) {
-  return a + b;
+  return parseInt(a) + parseInt(b);
 };
 
 const subtract = function (a, b) {
-  return a - b;
+  return parseInt(a) - parseInt(b);
 };
 
 const multiply = function (a, b) {
-  return a * b;
+  return parseInt(a) * parseInt(b);
 };
 
 const divide = function (a, b) {
   if (b === 0) return "ERROR";
-  else return a / b;
+  else return parseInt(a) / parseInt(b);;
 };
 
 function operate(operandOne, operandTwo, operator) {
@@ -42,4 +69,36 @@ function operate(operandOne, operandTwo, operator) {
       equate = 0;
   }
   return equate;
+}
+
+// Event handlers function
+function handleDigitClick(event) {
+  // Add digit value to operand one if operator is not yet clicked
+  if (!operatorClicked) {
+    operandOne += event.target.textContent;
+  } // otherwise add to operand two
+  else {
+    operandTwo += event.target.textContent;
+  }
+}
+
+function handleOperatorClick(event) {
+  operatorClicked = !operatorClicked;
+  operator = event.target.textContent;
+}
+
+function handleClearClick(){
+  operandOne = "";
+  operandTwo = "";
+  operator = "";
+  operatorClicked = false;
+}
+
+function handleEquateClick(){
+  display.textContent = operate(operandOne, operandTwo, operator);
+  handleClearClick();
+}
+
+function handleDisplayUpdate(){
+  display.textContent = `${operandOne} ${operator} ${operandTwo}`
 }
